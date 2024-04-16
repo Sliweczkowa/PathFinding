f9ead3ca322ea807ea3b1f7444e0933887d7074c
sliweczkowa <w.sliwinska@outlook.com>
2024-04-17 01:17:57 +0200

    feat: processed map display; refactor: hashmap moved out of function

--- src/mapOperations.jl
+++ src/mapOperations.jl
@@ -24,20 +24,23 @@ module mapOperations
         end
     end
 
+    hashmap = Dict{Union{Char, String}, RGB{N0f8}}(
+        '.' => colorant"white",
+        'G' => colorant"white",
+        '@' => colorant"black",
+        'O' => colorant"black",
+        'T' => colorant"green",
+        'S' => colorant"saddlebrown",
+        'W' => colorant"blue",
+        "considered" => colorant"pink1",
+        "visited" => colorant"pink2",
+        "path" => colorant"pink3"
+    )
+
     function mapToPNG(
         map::Array{Char, 2},
         filename::String
     )::Nothing
-
-        hashmap = Dict{Char, RGB{N0f8}}(
-            '.' => colorant"white",
-            'G' => colorant"white",
-            '@' => colorant"black",
-            'O' => colorant"black",
-            'T' => colorant"green",
-            'S' => colorant"saddlebrown",
-            'W' => colorant"blue"
-        )
 
         rgbMap = Array{RGB{N0f8}, 2}(undef, size(map)[1], size(map)[2])
         for i in 1:size(map)[1]
@@ -45,6 +48,37 @@ module mapOperations
                 rgbMap[i, j] = hashmap[map[i, j]]
             end
         end
+        save("out/$filename.png", rgbMap)
+
+    end
+
+    function mapToPNG(
+        mapInfo::Array{mapProperties.MapPoint, 2},
+        final_y::Int, final_x::Int,
+        filename::String
+    )::Nothing
+        
+        rgbMap = Array{RGB{N0f8}, 2}(undef, size(mapInfo)[1], size(mapInfo)[2])
+        for i in 1:size(mapInfo)[1]
+            for j in 1:size(mapInfo)[2]
+                if mapInfo[i, j].isVisited
+                    rgbMap[i, j] = hashmap["visited"]
+                elseif mapInfo[i, j].isConsidered
+                    rgbMap[i, j] = hashmap["considered"]
+                else
+                    rgbMap[i, j] = hashmap[mapInfo[i, j].terrainType]
+                end
+            end
+        end
+        current_y = final_y
+        current_x = final_x
+        while mapInfo[current_y, current_x].previousLocation != (0, 0)
+            rgbMap[current_y, current_x] = hashmap["path"]
+            y = current_y
+            current_y = mapInfo[current_y, current_x].previousLocation[1]
+            current_x = mapInfo[y, current_x].previousLocation[2]
+        end
+        rgbMap[current_y, current_x] = hashmap["path"]
         save("out/$filename.png", rgbMap)
 
     end
