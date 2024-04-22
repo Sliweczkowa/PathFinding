1946bec7e769ad203a4b52b8c8c6fa59d21d5909
sliweczkowa <w.sliwinska@outlook.com>
2024-04-22 23:31:13 +0200

    restyle: redundant tabulation

--- src/mapOperations.jl
+++ src/mapOperations.jl
@@ -1,84 +1,84 @@
-    using Images
+using Images
 
 
-    function loadMapFromFile(
-        pathToFile::String
-    )::Array{Char, 2}
+function loadMapFromFile(
+    pathToFile::String
+)::Array{Char, 2}
 
-        open(pathToFile) do fileWithMap        
-            readline(fileWithMap)
-            height = parse(Int32, readline(fileWithMap)[8:end])
-            width = parse(Int32, readline(fileWithMap)[7:end])
-            readline(fileWithMap)
-        
-            arrayWithMap = Array{Char, 2}(undef, height, width)
-        
-            for i in 1:height
-                arrayWithMap[i, 1:end] = collect(readline(fileWithMap))
-            end
+    open(pathToFile) do fileWithMap        
+        readline(fileWithMap)
+        height = parse(Int32, readline(fileWithMap)[8:end])
+        width = parse(Int32, readline(fileWithMap)[7:end])
+        readline(fileWithMap)
+    
+        arrayWithMap = Array{Char, 2}(undef, height, width)
+    
+        for i in 1:height
+            arrayWithMap[i, 1:end] = collect(readline(fileWithMap))
+        end
 
-            return arrayWithMap
+        return arrayWithMap
+    end
+end
+
+
+hashmap = Dict{Union{Char, String}, RGB{N0f8}}(
+    '.' => colorant"white",
+    'G' => colorant"white",
+    '@' => colorant"black",
+    'O' => colorant"black",
+    'T' => colorant"green",
+    'S' => colorant"saddlebrown",
+    'W' => colorant"blue",
+    "considered" => colorant"gray40",
+    "visited" => colorant"gray25",
+    "path" => colorant"gray10"
+)
+
+
+function mapToPNG(
+    map::Array{Char, 2},
+    filename::String
+)::Nothing
+
+    rgbMap = Array{RGB{N0f8}, 2}(undef, size(map)[1], size(map)[2])
+    for i in 1:size(map)[1]
+        for j in 1:size(map)[2]
+            rgbMap[i, j] = hashmap[map[i, j]]
         end
     end
+    save("out/$filename.png", rgbMap)
+
+end
 
 
-    hashmap = Dict{Union{Char, String}, RGB{N0f8}}(
-        '.' => colorant"white",
-        'G' => colorant"white",
-        '@' => colorant"black",
-        'O' => colorant"black",
-        'T' => colorant"green",
-        'S' => colorant"saddlebrown",
-        'W' => colorant"blue",
-        "considered" => colorant"gray40",
-        "visited" => colorant"gray25",
-        "path" => colorant"gray10"
-    )
-
-
-    function mapToPNG(
-        map::Array{Char, 2},
-        filename::String
-    )::Nothing
-
-        rgbMap = Array{RGB{N0f8}, 2}(undef, size(map)[1], size(map)[2])
-        for i in 1:size(map)[1]
-            for j in 1:size(map)[2]
-                rgbMap[i, j] = hashmap[map[i, j]]
+function mapToPNG(
+    mapInfo::Array{MapPoint, 2},
+    final_y::Int, final_x::Int,
+    filename::String
+)::Nothing
+    
+    rgbMap = Array{RGB{N0f8}, 2}(undef, size(mapInfo)[1], size(mapInfo)[2])
+    for i in 1:size(mapInfo)[1]
+        for j in 1:size(mapInfo)[2]
+            if mapInfo[i, j].isVisited
+                rgbMap[i, j] = hashmap["visited"]
+            elseif mapInfo[i, j].isConsidered
+                rgbMap[i, j] = hashmap["considered"]
+            else
+                rgbMap[i, j] = hashmap[mapInfo[i, j].terrainType]
             end
         end
-        save("out/$filename.png", rgbMap)
-
     end
-
-
-    function mapToPNG(
-        mapInfo::Array{MapPoint, 2},
-        final_y::Int, final_x::Int,
-        filename::String
-    )::Nothing
-        
-        rgbMap = Array{RGB{N0f8}, 2}(undef, size(mapInfo)[1], size(mapInfo)[2])
-        for i in 1:size(mapInfo)[1]
-            for j in 1:size(mapInfo)[2]
-                if mapInfo[i, j].isVisited
-                    rgbMap[i, j] = hashmap["visited"]
-                elseif mapInfo[i, j].isConsidered
-                    rgbMap[i, j] = hashmap["considered"]
-                else
-                    rgbMap[i, j] = hashmap[mapInfo[i, j].terrainType]
-                end
-            end
-        end
-        current_y = final_y
-        current_x = final_x
-        while mapInfo[current_y, current_x].previousLocation != (0, 0)
-            rgbMap[current_y, current_x] = hashmap["path"]
-            y = current_y
-            current_y = mapInfo[current_y, current_x].previousLocation[1]
-            current_x = mapInfo[y, current_x].previousLocation[2]
-        end
+    current_y = final_y
+    current_x = final_x
+    while mapInfo[current_y, current_x].previousLocation != (0, 0)
         rgbMap[current_y, current_x] = hashmap["path"]
-        save("out/$filename.png", rgbMap)
-
+        y = current_y
+        current_y = mapInfo[current_y, current_x].previousLocation[1]
+        current_x = mapInfo[y, current_x].previousLocation[2]
     end
+    rgbMap[current_y, current_x] = hashmap["path"]
+    save("out/$filename.png", rgbMap)
+
+end
--- src/pathFinding.jl
+++ src/pathFinding.jl
@@ -1,119 +1,119 @@
-    function waStar(
-        map::Array{Char, 2},
-        start_x::Int, start_y::Int,
-        final_x::Int, final_y::Int,
-        weight::Float64
-    )::Tuple{Array{MapPoint, 2}, Int, Int}
+function waStar(
+    map::Array{Char, 2},
+    start_x::Int, start_y::Int,
+    final_x::Int, final_y::Int,
+    weight::Float64
+)::Tuple{Array{MapPoint, 2}, Int, Int}
 
-        # Terrain start and finish check
-        if isTerrainExplorable(map[start_y, start_x]) == false || isTerrainExplorable(map[final_y, final_x]) == false
-            throw(ArgumentError("Invalid map coordinates: Unpassable terrain"))
-        end
+    # Terrain start and finish check
+    if isTerrainExplorable(map[start_y, start_x]) == false || isTerrainExplorable(map[final_y, final_x]) == false
+        throw(ArgumentError("Invalid map coordinates: Unpassable terrain"))
+    end
 
-        # Size for mapInfo
-        height = size(map, 1)
-        width = size(map, 2)
+    # Size for mapInfo
+    height = size(map, 1)
+    width = size(map, 2)
 
-        # Info about map initially unknown
-        mapInfo = Array{MapPoint, 2}(undef, height, width)
-        for i in 1:height
-            for j in 1:width
-                mapInfo[i, j] = MapPoint(map[i, j])
-                mapInfo[i, j].distanceToFinal = abs(final_x - j) + abs(final_y - i)
-            end  
-        end
+    # Info about map initially unknown
+    mapInfo = Array{MapPoint, 2}(undef, height, width)
+    for i in 1:height
+        for j in 1:width
+            mapInfo[i, j] = MapPoint(map[i, j])
+            mapInfo[i, j].distanceToFinal = abs(final_x - j) + abs(final_y - i)
+        end  
+    end
 
-        # Starting position
-        mapInfo[start_y, start_x].distanceFromStart = 0
+    # Starting position
+    mapInfo[start_y, start_x].distanceFromStart = 0
 
-        # Array of reachable points
-        reachableLocations = Array{Tuple{Int, Int}}(undef, 0)
-        push!(reachableLocations, (start_y, start_x))
+    # Array of reachable points
+    reachableLocations = Array{Tuple{Int, Int}}(undef, 0)
+    push!(reachableLocations, (start_y, start_x))
 
-        while !mapInfo[final_y, final_x].isVisited
-            
-            # Weight dependant choice
-            min_distance = (1 - weight) * (mapInfo[reachableLocations[1][1], reachableLocations[1][2]].distanceFromStart) + weight * (mapInfo[reachableLocations[1][1], reachableLocations[1][2]].distanceToFinal)
-            min_i = 1
-            for i in 2:size(reachableLocations, 1)
-                if min_distance > (1 - weight) * (mapInfo[reachableLocations[i][1], reachableLocations[i][2]].distanceFromStart) + weight * (mapInfo[reachableLocations[i][1], reachableLocations[i][2]].distanceToFinal)
-                    min_distance = (1 - weight) * (mapInfo[reachableLocations[i][1], reachableLocations[i][2]].distanceFromStart) + weight * (mapInfo[reachableLocations[i][1], reachableLocations[i][2]].distanceToFinal)
-                    min_i = i
-                end
-            end
-            current_x = reachableLocations[min_i][2]
-            current_y = reachableLocations[min_i][1]
-            mapInfo[current_y, current_x].isVisited = true
-
-            # Distance initialisation, Array push for currently reachable locations
-            if current_x-1 > 1 &&
-            isTerrainExplorable(mapInfo[current_y, current_x-1].terrainType) &&
-            mapInfo[current_y, current_x-1].isVisited == false &&
-            mapInfo[current_y, current_x-1].distanceFromStart > mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y, current_x-1].terrainType]
-                    mapInfo[current_y, current_x-1].distanceFromStart = mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y, current_x-1].terrainType]
-                    mapInfo[current_y, current_x-1].previousLocation = (current_y, current_x)
-                    push!(reachableLocations, (current_y, current_x-1))
-                    mapInfo[current_y, current_x-1].isConsidered = true
-            end
-            if current_y-1 > 1 &&
-                isTerrainExplorable(mapInfo[current_y-1, current_x].terrainType) &&
-                mapInfo[current_y-1, current_x].isVisited == false &&
-                mapInfo[current_y-1, current_x].distanceFromStart > mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y-1, current_x].terrainType]
-                        mapInfo[current_y-1, current_x].distanceFromStart = mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y-1, current_x].terrainType]
-                        mapInfo[current_y-1, current_x].previousLocation = (current_y, current_x)
-                        push!(reachableLocations, (current_y-1, current_x))
-                        mapInfo[current_y-1, current_x].isConsidered = true
-            end
-            if current_x+1 < width &&
-                isTerrainExplorable(mapInfo[current_y, current_x+1].terrainType) &&
-                mapInfo[current_y, current_x+1].isVisited == false &&
-                mapInfo[current_y, current_x+1].distanceFromStart > mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y, current_x+1].terrainType]
-                        mapInfo[current_y, current_x+1].distanceFromStart = mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y, current_x+1].terrainType]
-                        mapInfo[current_y, current_x+1].previousLocation = (current_y, current_x)
-                        push!(reachableLocations, (current_y, current_x+1))
-                        mapInfo[current_y, current_x+1].isConsidered = true
-            end
-            if current_y+1 < height &&
-                isTerrainExplorable(mapInfo[current_y+1, current_x].terrainType) &&
-                mapInfo[current_y+1, current_x].isVisited == false &&
-                mapInfo[current_y+1, current_x].distanceFromStart > mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y+1, current_x].terrainType]
-                        mapInfo[current_y+1, current_x].distanceFromStart = mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y+1, current_x].terrainType]
-                        mapInfo[current_y+1, current_x].previousLocation = (current_y, current_x)
-                        push!(reachableLocations, (current_y+1, current_x))
-                        mapInfo[current_y+1, current_x].isConsidered = true
-            end
-
-            # Analyse of current location finished
-            deleteat!(reachableLocations, min_i)
-            mapInfo[current_y, current_x].isVisited = true
-
-        end
-
-        if mapInfo[final_y, final_x].distanceFromStart  != Inf32
-            return mapInfo, final_y, final_x
-        else
-            throw(ArgumentError("No possible path"))
-        end
+    while !mapInfo[final_y, final_x].isVisited
         
-    end
-
-
-    function isTerrainExplorable(c::Char)::Bool
-        if c == '@' || c == 'O' || c == 'T'
-            return false
-        elseif c == '.' || c == 'G' || c == 'S' || c == 'W'
-            return true
-        else
-            throw(ArgumentError("Invalid map argument"))
+        # Weight dependant choice
+        min_distance = (1 - weight) * (mapInfo[reachableLocations[1][1], reachableLocations[1][2]].distanceFromStart) + weight * (mapInfo[reachableLocations[1][1], reachableLocations[1][2]].distanceToFinal)
+        min_i = 1
+        for i in 2:size(reachableLocations, 1)
+            if min_distance > (1 - weight) * (mapInfo[reachableLocations[i][1], reachableLocations[i][2]].distanceFromStart) + weight * (mapInfo[reachableLocations[i][1], reachableLocations[i][2]].distanceToFinal)
+                min_distance = (1 - weight) * (mapInfo[reachableLocations[i][1], reachableLocations[i][2]].distanceFromStart) + weight * (mapInfo[reachableLocations[i][1], reachableLocations[i][2]].distanceToFinal)
+                min_i = i
+            end
         end
+        current_x = reachableLocations[min_i][2]
+        current_y = reachableLocations[min_i][1]
+        mapInfo[current_y, current_x].isVisited = true
+
+        # Distance initialisation, Array push for currently reachable locations
+        if current_x-1 > 1 &&
+        isTerrainExplorable(mapInfo[current_y, current_x-1].terrainType) &&
+        mapInfo[current_y, current_x-1].isVisited == false &&
+        mapInfo[current_y, current_x-1].distanceFromStart > mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y, current_x-1].terrainType]
+                mapInfo[current_y, current_x-1].distanceFromStart = mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y, current_x-1].terrainType]
+                mapInfo[current_y, current_x-1].previousLocation = (current_y, current_x)
+                push!(reachableLocations, (current_y, current_x-1))
+                mapInfo[current_y, current_x-1].isConsidered = true
+        end
+        if current_y-1 > 1 &&
+            isTerrainExplorable(mapInfo[current_y-1, current_x].terrainType) &&
+            mapInfo[current_y-1, current_x].isVisited == false &&
+            mapInfo[current_y-1, current_x].distanceFromStart > mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y-1, current_x].terrainType]
+                    mapInfo[current_y-1, current_x].distanceFromStart = mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y-1, current_x].terrainType]
+                    mapInfo[current_y-1, current_x].previousLocation = (current_y, current_x)
+                    push!(reachableLocations, (current_y-1, current_x))
+                    mapInfo[current_y-1, current_x].isConsidered = true
+        end
+        if current_x+1 < width &&
+            isTerrainExplorable(mapInfo[current_y, current_x+1].terrainType) &&
+            mapInfo[current_y, current_x+1].isVisited == false &&
+            mapInfo[current_y, current_x+1].distanceFromStart > mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y, current_x+1].terrainType]
+                    mapInfo[current_y, current_x+1].distanceFromStart = mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y, current_x+1].terrainType]
+                    mapInfo[current_y, current_x+1].previousLocation = (current_y, current_x)
+                    push!(reachableLocations, (current_y, current_x+1))
+                    mapInfo[current_y, current_x+1].isConsidered = true
+        end
+        if current_y+1 < height &&
+            isTerrainExplorable(mapInfo[current_y+1, current_x].terrainType) &&
+            mapInfo[current_y+1, current_x].isVisited == false &&
+            mapInfo[current_y+1, current_x].distanceFromStart > mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y+1, current_x].terrainType]
+                    mapInfo[current_y+1, current_x].distanceFromStart = mapInfo[current_y, current_x].distanceFromStart + hashmap1[mapInfo[current_y+1, current_x].terrainType]
+                    mapInfo[current_y+1, current_x].previousLocation = (current_y, current_x)
+                    push!(reachableLocations, (current_y+1, current_x))
+                    mapInfo[current_y+1, current_x].isConsidered = true
+        end
+
+        # Analyse of current location finished
+        deleteat!(reachableLocations, min_i)
+        mapInfo[current_y, current_x].isVisited = true
+
     end
 
-    hashmap1 = Dict{Char, Float64}(
-        '.' => 1.0,
-        'G' => 1.0,
-        '@' => Inf64,
-        'O' => Inf64,
-        'T' => Inf64,
-        'S' => 5.0,
-        'W' => 8.0
-    )
+    if mapInfo[final_y, final_x].distanceFromStart  != Inf32
+        return mapInfo, final_y, final_x
+    else
+        throw(ArgumentError("No possible path"))
+    end
+    
+end
+
+
+function isTerrainExplorable(c::Char)::Bool
+    if c == '@' || c == 'O' || c == 'T'
+        return false
+    elseif c == '.' || c == 'G' || c == 'S' || c == 'W'
+        return true
+    else
+        throw(ArgumentError("Invalid map argument"))
+    end
+end
+
+hashmap1 = Dict{Char, Float64}(
+    '.' => 1.0,
+    'G' => 1.0,
+    '@' => Inf64,
+    'O' => Inf64,
+    'T' => Inf64,
+    'S' => 5.0,
+    'W' => 8.0
+)
